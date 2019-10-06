b310b202cb161d7403a6ceb1cc2bb280c5ea4acd
Nick Sherman <n.e.sherman@berkeley.edu>
2019-10-06 14:44:03 -0700

    Chebyshev method implemented

--- model/heisenberglouiville.h
+++ model/heisenberglouiville.h
@@ -17,22 +17,22 @@ protected:
 	virtual void calcH()
 	{
 		auto ampo = AutoMPO(sites);
-        for(auto b : *lattice)
-        {
-        	if(b.t == Lattice::physical)
-        	{
+	    for(auto b : *lattice)
+	    {
+	    	if(b.t == Lattice::physical)
+	    	{
 	            ampo += 0.5,"S+",b.s1,"S-",b.s2;
 	            ampo += 0.5,"S-",b.s1,"S+",b.s2;
 	            ampo += 1.0,"Sz",b.s1,"Sz",b.s2;
-        	}
-        	if(b.t == Lattice::environment)
-        	{
-	            ampoE += -0.5,"S+",b.s1,"S-",b.s2;
-	            ampoE += -0.5,"S-",b.s1,"S+",b.s2;
-	            ampoE += -1.0,"Sz",b.s1,"Sz",b.s2;
-        	}
-        }
-        H = toMPO(ampo);
+	    	}
+	    	if(b.t == Lattice::environment)
+	    	{
+	            ampo += -0.5,"S+",b.s1,"S-",b.s2;
+	            ampo += -0.5,"S-",b.s1,"S+",b.s2;
+	            ampo += -1.0,"Sz",b.s1,"Sz",b.s2;
+	    	}
+	    }
+	    H = toMPO(ampo);
 	}
 public:
 	HeisenbergLouiville(){}
